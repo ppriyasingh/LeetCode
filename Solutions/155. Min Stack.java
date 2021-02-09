093bfa2d2d78f196c6ba6bbf71cbfd409474c617
Priyanka Singh <priyanka.singh@cerence.com>
2021-02-09 18:04:15 +0530

    155 optimised

--- Solutions/155. Min Stack.java
+++ Solutions/155. Min Stack.java
@@ -1,3 +1,53 @@
+/**** OPTIMISED SOLUTION with single stack *****/
+
+class MinStack {
+
+    Stack<Integer> stack;
+    int min;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) {
+        if(x<=min) {
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        if(min == stack.pop()) {
+            min=stack.pop();
+        }
+    }
+    
+    public int top() {
+        
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+
+
+/**** Two stack solution*****/ 
+
+
 class MinStack {
 
     Stack<Integer> s;    
@@ -40,3 +90,5 @@ class MinStack {
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+
+
