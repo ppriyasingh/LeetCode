28bc6b3c45cc0b6f792842225b422eed412e7fee
Priyanka Singh <priyankahoney2110@gmail.com>
2022-04-24 18:54:01 +0530

    update

--- Solutions/Locked Premium/772. Basic Calculator III.java
+++ Solutions/Locked Premium/772. Basic Calculator III.java
@@ -20,19 +20,21 @@ Input: s = "2*(5+5*2)/3+(6/2+8)"
 Output: 21
 */
 
-public static int calculate(String s) {
-        if (s == null || s.length() == 0) return 0;
-        Stack<Integer> nums = new Stack<>(); // the stack that stores numbers
-        Stack<Character> ops = new Stack<>(); // the stack that stores operators (including parentheses)
+public int calculate(String s) {
+        //time:O(N), space:O(N)
+        if (s == null || s.length() == 0) return 0; 
+        Stack<Integer> nums = new Stack<>();    // the stack that stores numbers
+        Stack<Character> ops = new Stack<>();   // the stack that stores operators (including parentheses)
         int num = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (c == ' ') continue;
+            if (c == ' ') {
+                continue;
+            }
             if (Character.isDigit(c)) {
                 num = c - '0';
-                // iteratively calculate each number
-                while (i < s.length() - 1 && Character.isDigit(s.charAt(i+1))) {
-                    num = num * 10 + (s.charAt(i+1) - '0');
+                while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    num = 10 * num + (s.charAt(i + 1) - '0');
                     i++;
                 }
                 nums.push(num);
@@ -40,32 +42,53 @@ public static int calculate(String s) {
             } else if (c == '(') {
                 ops.push(c);
             } else if (c == ')') {
-                // do the math when we encounter a ')' until '('
-                while (ops.peek() != '(') nums.push(operation(ops.pop(), nums.pop(), nums.pop()));
-                ops.pop(); // get rid of '(' in the ops stack
+                while (ops.peek() != '(') {
+                    nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
+                }
+                ops.pop();
             } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                while (!ops.isEmpty() && precedence(c, ops.peek())) nums.push(operation(ops.pop(), nums.pop(),nums.pop()));
+                while (!ops.isEmpty() && hasPrecedence(ops.peek(), c)) {
+                    nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
+                }
+                // Dealing with the negative number
+                if (c == '-') {
+                    if (nums.isEmpty()) { // case1: 1st non-empty characer is the negative number 
+                        nums.push(0);
+                    } else { // case2: 1st non-empty characer in parentheses is the negative number 
+                        int index = i - 1;
+                        while (index >= 0 && s.charAt(index) == ' ') {
+                            index--;
+                        }
+                        if (s.charAt(index) == '(') {
+                            nums.push(0);
+                        }
+                    }
+                }
                 ops.push(c);
             }
         }
         while (!ops.isEmpty()) {
-            nums.push(operation(ops.pop(), nums.pop(), nums.pop()));
+           nums.push(operation(nums.pop(), ops.pop(), nums.pop()));
         }
         return nums.pop();
     }
-
-    private static int operation(char op, int b, int a) {
+    // Notice b is before a, since we pop b first
+    private int operation(int b, char op, int a) {
         switch (op) {
             case '+': return a + b;
             case '-': return a - b;
             case '*': return a * b;
-            case '/': return a / b; // assume b is not 0
+            case '/': return a / b; //assume b is not 0
         }
         return 0;
     }
-    // helper function to check precedence of current operator and the uppermost operator in the ops stack 
-    private static boolean precedence(char op1, char op2) {
-        if (op2 == '(' || op2 == ')') return false;
-        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-')) return false;
+    // helper function to check precedence of the uppermost operator in the ops stack and current operator
+    private boolean hasPrecedence(char op1, char op2) {
+        if (op1 == '(' || op1 == ')') {
+            return false;
+        }
+        if ((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/')) {
+            return false;
+        }
         return true;
     }
