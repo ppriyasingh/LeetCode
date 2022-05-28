fc1e864d6d625eaa3aa71d4e31888243fe4b920a
Priyanka Singh <priyankahoney2110@gmail.com>
2022-05-28 17:08:19 +0530

    Create 152. Maximum Product Subarray.java

---
+++ Solutions/152. Maximum Product Subarray.java
@@ -0,0 +1,46 @@
+// Approach 1: Brute Force
+class Solution {
+// T(O): O(N^2)
+// S(O): O(1)
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int result = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            int accu = 1;
+            for (int j = i; j < nums.length; j++) {
+                accu *= nums[j];
+                result = Math.max(result, accu);
+            }
+        }
+
+        return result;
+    }
+}
+
+
+// Approach 2: Dynamic Programming
+class Solution {
+// T(O): O(N)
+// S(O): O(1)
+    public int maxProduct(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int max_so_far = nums[0];
+        int min_so_far = nums[0];
+        int result = max_so_far;
+
+        for (int i = 1; i < nums.length; i++) {
+            int curr = nums[i];
+            int temp_max = Math.max(curr, Math.max(max_so_far * curr, min_so_far * curr));
+            min_so_far = Math.min(curr, Math.min(max_so_far * curr, min_so_far * curr));
+
+            max_so_far = temp_max;
+
+            result = Math.max(max_so_far, result);
+        }
+
+        return result;
+    }
+}
