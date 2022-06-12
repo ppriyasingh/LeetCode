16aff60ac2b5ebcf645c53155ff4fb58d31e5900
Priyanka Singh <priyankahoney2110@gmail.com>
2022-06-12 19:30:17 +0530

    Update 200. Number of Islands.java

--- Solutions/200. Number of Islands.java
+++ Solutions/200. Number of Islands.java
@@ -38,6 +38,35 @@ class Solution {
   }
 }
 
+// Different way:
+class Solution {
+    int[][] dir = new int[][] {{1,0}, {-1,0}, {0,-1}, {0,1}};
+    public void dfs(char[][] grid, int i, int j) {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]=='0') return;
+        
+        grid[i][j] = '0';
+        for(int k=0; k<4; k++) {
+            int x = i+dir[k][0], y = j+dir[k][1];
+            dfs(grid, x, y);
+        }
+    }
+    
+    public int numIslands(char[][] grid) {
+     
+        int count=0;
+        
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j] =='1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
 // Approach #2: BFS [Accepted]
 class Solution {
   //   T(O): O(M*N)
