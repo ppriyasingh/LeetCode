d0e605452af33358ad9809dd08ec40364ec098e0
Priyanka Singh <priyankahoney2110@gmail.com>
2022-06-12 11:45:26 +0530

    Create 200. Number of Islands.java

---
+++ Solutions/200. Number of Islands.java
@@ -0,0 +1,179 @@
+// Approach #1 DFS [Accepted]
+class Solution {
+  //   T(O): O(M*N)
+  //   S(O): worst case if visited grid maintained or in case that the grid map is filled with lands where DFS goes by M×N deep.: O(M*N)
+  void dfs(char[][] grid, int r, int c) {
+    int nr = grid.length;
+    int nc = grid[0].length;
+
+    if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+      return;
+    }
+
+    grid[r][c] = '0';
+    dfs(grid, r - 1, c);
+    dfs(grid, r + 1, c);
+    dfs(grid, r, c - 1);
+    dfs(grid, r, c + 1);
+  }
+
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          dfs(grid, r, c);
+        }
+      }
+    }
+
+    return num_islands;
+  }
+}
+
+// Approach #2: BFS [Accepted]
+class Solution {
+  //   T(O): O(M*N)
+  //   S(O): O(min(M,N)), because in worst case where the grid is filled with lands, the size of queue can grow up to min(M,N).
+  
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          ++num_islands;
+          grid[r][c] = '0'; // mark as visited
+          Queue<Integer> neighbors = new LinkedList<>();
+          neighbors.add(r * nc + c);
+          while (!neighbors.isEmpty()) {
+            int id = neighbors.remove();
+            int row = id / nc;
+            int col = id % nc;
+            if (row - 1 >= 0 && grid[row-1][col] == '1') {
+              neighbors.add((row-1) * nc + col);
+              grid[row-1][col] = '0';
+            }
+            if (row + 1 < nr && grid[row+1][col] == '1') {
+              neighbors.add((row+1) * nc + col);
+              grid[row+1][col] = '0';
+            }
+            if (col - 1 >= 0 && grid[row][col-1] == '1') {
+              neighbors.add(row * nc + col-1);
+              grid[row][col-1] = '0';
+            }
+            if (col + 1 < nc && grid[row][col+1] == '1') {
+              neighbors.add(row * nc + col+1);
+              grid[row][col+1] = '0';
+            }
+          }
+        }
+      }
+    }
+
+    return num_islands;
+  }
+}
+
+// Approach #3: Union Find (aka Disjoint Set) [Accepted]
+/**
+Algorithm
+
+Traverse the 2d grid map and union adjacent lands horizontally or vertically, at the end, return the number of connected components maintained in the 
+UnionFind data structure.
+*/
+class Solution {
+  //   T(O): O(M*N)
+  //   S(O): O(M*N)
+  class UnionFind {
+    int count; // # of connected components
+    int[] parent;
+    int[] rank;
+
+    public UnionFind(char[][] grid) { // for problem 200
+      count = 0;
+      int m = grid.length;
+      int n = grid[0].length;
+      parent = new int[m * n];
+      rank = new int[m * n];
+      for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+          if (grid[i][j] == '1') {
+            parent[i * n + j] = i * n + j;
+            ++count;
+          }
+          rank[i * n + j] = 0;
+        }
+      }
+    }
+
+    public int find(int i) { // path compression
+      if (parent[i] != i) parent[i] = find(parent[i]);
+      return parent[i];
+    }
+
+    public void union(int x, int y) { // union with rank
+      int rootx = find(x);
+      int rooty = find(y);
+      if (rootx != rooty) {
+        if (rank[rootx] > rank[rooty]) {
+          parent[rooty] = rootx;
+        } else if (rank[rootx] < rank[rooty]) {
+          parent[rootx] = rooty;
+        } else {
+          parent[rooty] = rootx; rank[rootx] += 1;
+        }
+        --count;
+      }
+    }
+
+    public int getCount() {
+      return count;
+    }
+  }
+
+  public int numIslands(char[][] grid) {
+    if (grid == null || grid.length == 0) {
+      return 0;
+    }
+
+    int nr = grid.length;
+    int nc = grid[0].length;
+    int num_islands = 0;
+    UnionFind uf = new UnionFind(grid);
+    for (int r = 0; r < nr; ++r) {
+      for (int c = 0; c < nc; ++c) {
+        if (grid[r][c] == '1') {
+          grid[r][c] = '0';
+          if (r - 1 >= 0 && grid[r-1][c] == '1') {
+            uf.union(r * nc + c, (r-1) * nc + c);
+          }
+          if (r + 1 < nr && grid[r+1][c] == '1') {
+            uf.union(r * nc + c, (r+1) * nc + c);
+          }
+          if (c - 1 >= 0 && grid[r][c-1] == '1') {
+            uf.union(r * nc + c, r * nc + c - 1);
+          }
+          if (c + 1 < nc && grid[r][c+1] == '1') {
+            uf.union(r * nc + c, r * nc + c + 1);
+          }
+        }
+      }
+    }
+
+    return uf.getCount();
+  }
+}
