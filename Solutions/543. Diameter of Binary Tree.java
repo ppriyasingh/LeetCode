c2a3e81a996d520faac007992c631df7b056ce0c
Priyanka Singh <priyankahoney2110@gmail.com>
2022-06-13 10:20:44 +0530

    Create 543. Diameter of Binary Tree.java

---
+++ Solutions/543. Diameter of Binary Tree.java
@@ -0,0 +1,24 @@
+class Solution {
+//   T(O): O(N)
+//   S(O): O(N)
+    private int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameter = 0;
+        longestPath(root);
+        return diameter;
+    }
+    private int longestPath(TreeNode node){
+        if(node == null) return 0;
+        // recursively find the longest path in
+        // both left child and right child
+        int leftPath = longestPath(node.left);
+        int rightPath = longestPath(node.right);
+
+        // update the diameter if left_path plus right_path is larger
+        diameter = Math.max(diameter, leftPath + rightPath);
+
+        // return the longest one between left_path and right_path;
+        // remember to add 1 for the path connecting the node and its parent
+        return Math.max(leftPath, rightPath) + 1;
+    }
+}
