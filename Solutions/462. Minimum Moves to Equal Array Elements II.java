e50eff53c9a44425a0b8c19a49d3d63674f12534
Priyanka Singh <priyankahoney2110@gmail.com>
2022-06-30 06:44:37 +0530

    Create 462. Minimum Moves to Equal Array Elements II.java

---
+++ Solutions/462. Minimum Moves to Equal Array Elements II.java
@@ -0,0 +1,26 @@
+// Sol 1:
+public class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length-1;
+        int count = 0;
+        while(i < j){
+            count += nums[j]-nums[i];
+            i++;
+            j--;
+        }
+        return count;
+    }
+}
+
+// Sol 2:
+public class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int num : nums) {
+            sum += Math.abs(nums[nums.length / 2] - num);
+        }
+        return sum;
+    }
+}
