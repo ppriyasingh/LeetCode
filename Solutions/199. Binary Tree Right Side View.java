ef8e81b802a592569b1aa014a3e21cfade7700ee
Priyanka Singh <priyankahoney2110@gmail.com>
2022-07-11 14:31:49 +0530

    Update 199. Binary Tree Right Side View.java

--- Solutions/199. Binary Tree Right Side View.java
+++ Solutions/199. Binary Tree Right Side View.java
@@ -13,6 +13,8 @@
  *     }
  * }
  */
+
+// Best Solution: DFS
 class Solution {
     List<Integer> res = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
@@ -28,5 +30,30 @@ class Solution {
             
         if(root.right!=null) rightSideViewUtil(root.right, level+1);
         if(root.left!=null) rightSideViewUtil(root.left, level+1);
+    }
+}
+
+// BFS
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if(root == null ) return result;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.add(root);
+        
+        while(!q.isEmpty()) {
+            
+            int size = q.size();
+            
+            for(int i=0; i<size; i++) {
+                TreeNode temp = q.poll();
+                if(i == size-1) result.add(temp.val);
+                if(temp.left != null) q.add(temp.left);
+                if(temp.right != null) q.add(temp.right);
+            }
+        }
+        return result;
     }
 }
